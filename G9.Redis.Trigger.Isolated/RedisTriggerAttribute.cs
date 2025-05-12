edb7906db4852dd27212620928e8904c00ef3393
Mateusz Kłosiński <Mateusz.Klosinski@gravity9.com>
2025-05-12 22:53:57 +0200

    nuget pipeline

---
+++ G9.Redis.Trigger.Isolated/RedisTriggerAttribute.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.Azure.Functions.Worker.Extensions.Abstractions;
+
+[assembly: ExtensionInformation("G9.Redis.Trigger", "1.0.0")]
+
+namespace G9.Redis.Trigger.Isolated;
+
+[AttributeUsage(AttributeTargets.Parameter)]
+public sealed class RedisTriggerAttribute : TriggerBindingAttribute
+{
+    public string Channel { get; }
+
+    public RedisTriggerAttribute(string channel)
+    {
+		Channel = channel;
+    }
+}
--- G9.Redis.Trigger/RedisBindingStartupExtension.cs
+++ G9.Redis.Trigger/RedisBindingStartupExtension.cs
@@ -3,7 +3,6 @@ using Microsoft.Azure.WebJobs;
 using Microsoft.Azure.WebJobs.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Logging;
 using StackExchange.Redis;
 
 [assembly: WebJobsStartup(typeof(RedisBindingStartupExtension))]
